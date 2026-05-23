11f52abe43e4818bd2a8a5fb736c9a2d16d5350e
121212castro <121212castro@gmail.com>
2026-05-23 15:30:33 +0200

    Update app-loader-clean.js

--- app-loader-clean.js
+++ app-loader-clean.js
@@ -1,7 +1,8 @@
 window.AcuarioNexoLoaderClean={version:'loader-clean-23-05-openai-v2'};
 (function(){
   var build='loader-clean-23-05-openai-v2-'+Date.now();
-  var files=['version-engine.js','cache-engine.js','state-engine.js','navigation-engine.js','fichas-engine.js','crop-visual-engine.js','product-crop-engine.js','ai-engine.js','gemini-engine.js','inventario-engine.js','microfauna-engine.js','parameters-engine.js','nav-visual-fix.js'];
+  var files=['version-engine.js','cache-engine.js','state-engine.js','navigation-engine.js','fichas-engine.js','crop-visual-engine.js','product-crop-engine.js','ai-engine.js','gemini-engine.js','inventario-engine.js','microfauna-engine.js','parameters-engine.js',,
+             'admin-engine.js','nav-visual-fix.js'];
   for(var i=0;i<files.length;i++){
     var s=document.createElement('script');
     s.src=files[i]+'?v='+build;
