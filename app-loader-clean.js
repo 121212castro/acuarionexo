bc120bc1bbbbe152619903cdac73ef18078a0741
121212castro <121212castro@gmail.com>
2026-05-23 11:14:17 +0200

    Crear cargador limpio principal

---
+++ app-loader-clean.js
@@ -0,0 +1,9 @@
+window.AcuarioNexoLoaderClean={version:'loader-clean-23-05'};
+(function(){
+  var files=['version-engine.js','navigation-engine.js','fichas-engine.js','inventario-engine.js','microfauna-engine.js'];
+  for(var i=0;i<files.length;i++){
+    var s=document.createElement('script');
+    s.src=files[i]+'?v=loader-clean-23-05';
+    document.body.appendChild(s);
+  }
+})();
