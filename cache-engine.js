58abaf6a7974e2ce0aaa66e7f0187416bb460505
121212castro <121212castro@gmail.com>
2026-05-23 11:29:05 +0200

    Crear cache engine limpio

---
+++ cache-engine.js
@@ -0,0 +1,3 @@
+window.AcuarioNexoCache={version:'cache-clean-23-05'};
+window.AcuarioNexoCache.refresh=function(){location.replace(location.pathname+'?v='+Date.now())};
+window.hardRefreshAcuarioNexo=window.AcuarioNexoCache.refresh;
