be23d516cde4ea7f1d8155dc01bc466b73bd8c5a
121212castro <121212castro@gmail.com>
2026-05-25 01:07:00 +0200

    eliminar

--- cache-engine.js
+++
@@ -1,3 +0,0 @@
-window.AcuarioNexoCache={version:'cache-clean-23-05'};
-window.AcuarioNexoCache.refresh=function(){location.replace(location.pathname+'?v='+Date.now())};
-window.hardRefreshAcuarioNexo=window.AcuarioNexoCache.refresh;
