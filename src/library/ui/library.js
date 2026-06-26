99b42782187b63c14be34e2e472a344d3d47524a
121212castro <121212castro@gmail.com>
2026-06-27 01:39:03 +0200

    Fase 2 Biblioteca: crear puente UI para library.js

---
+++ src/library/ui/library.js
@@ -0,0 +1,4 @@
+/* AcuarioNexo · Biblioteca oficial · puente de carga Fase 2 */
+(function () {
+  document.write('<script src="src/library/library.js?v=' + encodeURIComponent(window.ACUARIONEXO_BUILD || 'library-v3b-20260624') + '"><\/script>');
+})();
