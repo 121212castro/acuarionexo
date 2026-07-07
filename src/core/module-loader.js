3f517aa4be040333ba81b2a3a0ffc7f3434427d7
121212castro <121212castro@gmail.com>
2026-07-08 01:55:15 +0200

    Load clean ficha actions module

--- src/core/module-loader.js
+++ src/core/module-loader.js
@@ -17,6 +17,7 @@
       'src/library/library-v3-images.js',
       'src/library/library-v3-ai.js',
       'src/library/library-v3-ficha.js',
+      'src/library/ficha/ficha-actions.js',
       'src/library/library-v3.js',
       'src/library/ficha/ficha-type-tools.js',
       'src/library/ficha/ficha-chat-import.js',
