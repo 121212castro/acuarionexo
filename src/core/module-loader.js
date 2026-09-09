a6f91a61eaee4c7356f93bed9f60abafacb7db1a
121212castro <121212castro@gmail.com>
2026-09-09 14:19:28 +0200

    Load 3D aquarium builder module

--- src/core/module-loader.js
+++ src/core/module-loader.js
@@ -33,6 +33,7 @@
       'src/map/map-v3-model.js',
       'src/map/map-state.js',
       'src/map/map-model-families.js',
+      'src/map/map-builder.js',
       'src/map/map-ui.js',
       'src/map/map-photos.js',
       'src/map/map-markers.js',
